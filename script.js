c7b558a071147490ac9165bdcb8e46edb0fe4bba
rcruz-main <cruz.rainierlaroya@gmail.com>
2021-04-09 22:54:26 +0800

    1st Commit for publishing
    for INFO 1213 project

---
+++ script.js
@@ -0,0 +1,39 @@
+ var MenuItems = document.getElementById("MenuItems")
+
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle(){
+  if(MenuItems.style.maxHeight == "0px"){
+      MenuItems.style.maxHeight = "200px"
+    }
+  else {
+    MenuItems.style.maxHeight = "0px"
+  }
+}
+
+var LoginForm = document.getElementById("LoginForm")
+var RegForm = document.getElementById("RegForm")
+var Indicator = document.getElementById("Indicator")
+
+  function register(){
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)";
+    Indicator.style.transform = "translateX(100px)";
+  }
+
+  function login(){
+    RegForm.style.transform = "translateX(300px)";
+    LoginForm.style.transform = "translateX(300px)";
+    Indicator.style.transform = "translateX(0px)";
+  }
+
+//this section is for the cart page
+var removeCartItemsButtons = document.getElementsByClassName('btn-danger')
+console.log(removeCartItemsButtons)
+
+for (var i = 0; i < removeCartItemsButtons.lenght; i++){
+  var button = removeCartItemsButtons[i]
+  button.addEventListener('click', function(){
+    console.log(clicked)
+  })
+}
